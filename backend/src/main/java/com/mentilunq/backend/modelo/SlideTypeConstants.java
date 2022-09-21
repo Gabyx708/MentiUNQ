fe90f3e0ff15c7334d16cecd18d3b18e0a669363
Henry Borda <henryborda17@gmail.com>
2022-09-21 09:24:31 -0300

    test: Agrego tests en back. Y opciones en front.

---
+++ backend/src/main/java/com/mentilunq/backend/modelo/SlideTypeConstants.java
@@ -0,0 +1,13 @@
+package com.mentilunq.backend.modelo;
+
+public interface SlideTypeConstants {
+    String WORD_CLOUD_NAME = "Word Cloud";
+    String WORD_CLOUD_CODE = "WORCLO";
+    String MULTIPLE_CHOICE_NAME = "Multiple Choice";
+    String MULTIPLE_CHOICE_CODE = "MULCHO";
+    String OPEN_ENDED_NAME = "Open Ended";
+    String OPEN_ENDED_CODE = "OPENEND";
+    String BLANK_NAME = "Sin Tipo";
+    String BLANK_CODE = "SINTIPO";
+    long ID_BLANK = 1;
+}
