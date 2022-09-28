f5455b4d7d480435910f8889fad1e3279859379c
Gaby <matias427coria@gmail.com>
2022-09-28 16:00:48 -0300

    mejoras en el sistema de autenticacion , la base de datos registra el usuario usando el boton de inicio de sesion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Home from "./pages/Home/index";
 import { Route, Routes } from "react-router-dom";
 import PresentationView from "./pages/PresentationView/PresentationView.jsx"
 import CreatePresentation from "./pages/CreatePresentation/index.jsx";
+
+
 export default function App() {
   return (
     <div className="App">
--- frontend/src/components/Login/LoginButton.jsx
+++ frontend/src/components/Login/LoginButton.jsx
@@ -10,5 +10,6 @@ export default function LoginButton() {
         Iniciar sesión
         <FiLogIn />
       </button>
+   
   );
 }
