8810ec1d8796ebf3a561aa1c85e28211b0345ba0
Gaby <matias427coria@gmail.com>
2022-10-26 23:02:28 -0300

    creacion de metodo para manejar la logica del ranking por parte del backend

--- backend/README.md
+++ backend/README.md
@@ -1,5 +1,10 @@
 # MentiUNQ - API
 
+## Levantar aplicación
+Deberá primero correr el comando `docker compose up` manualmente y luego desde IDEA
+
+![img.png](src/main/resources/img.png) 
+
 ## Swagger
 Acceder luego de levantar la app a: http://localhost:8080/swagger-ui.html
 
@@ -7,3 +12,7 @@ Acceder luego de levantar la app a: http://localhost:8080/swagger-ui.html
 - Java SDK 11
 - Springboot 2.7.3
 - Maven 3.8.6
+
+## Docker 
+Para levantar el contenedor de MySQL debemor correr `docker compose up` . Podemos configurar DataGrip para crear la conexión:
+![img_1.png](src/main/resources/img_1.png)
--- backend/src/main/java/com/mentilunq/backend/modelo/Ranking.java
+++ backend/src/main/java/com/mentilunq/backend/modelo/Ranking.java
@@ -1,8 +1,39 @@
 package com.mentilunq.backend.modelo;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+
 public class Ranking extends Slide {
 
+    private List<String> palabras;
     public Ranking(){
+
         super("Ranking");
+        palabras = new List<String>;
+    }
+
+    public Hashtable calcularPorcentaje(){
+
+        Hashtable<String , Integer> resultados = new Hashtable<String , Integer>();
+        int contador = 0;
+
+        for (String palabraComparar:palabras) {
+
+                for(String palabraAux:palabras) {
+                    if (palabraAux == palabraComparar) {
+                        contador++;
+                    }
+                }
+                resultados.put(palabraComparar,contador);
+                contador = 0;
+        }
+        return resultados; //el front debera trabajar con el diccionario para representar los datos
+    }
+
+    public void agregarPalabra(String p){
+       palabras.add(p);
     }
 }
+
+
